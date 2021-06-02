700543ce21c9b156274cff997a0fae25b11d558e
ehsan123551 <meqadir.bscs18seecs@seecs.edu.pk>
2021-06-02 20:57:28 +0500

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2020 Brought To You By<a href="https://code-projects.org/">Muhammad Ehsan Qadir</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 Brought to You By <a href="https://github.com/ehsan123551">Muhammad Ehsan Qadir</a></strong>
+    </div>
+</footer>
